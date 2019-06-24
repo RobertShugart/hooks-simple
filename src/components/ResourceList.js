31d168932effa963c033325105a91628d874343a
RobertS <rshugart@protonmail.com>
2019-06-24 16:08:59 -0400

    Class Based

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -2,12 +2,24 @@ import React from 'react';
 import axios from 'axios';
 
 class ResourceList extends React.Component {
-	componentDidMount() {
-		axios.get('https://jsonplaceholder.typicode.com');
+	state = { resource: [] };
+
+	async componentDidMount() {
+		const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+
+		this.setState({ resource: response.data });
+	}
+
+	async componentDidUpdate(prevProps) {
+		if (prevProps.resource !== this.props.resource) {
+			const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+
+			this.setState({ resource: response.data });
+		}
 	}
 
 	render() {
-		return <div>{this.props.resource}</div>;
+		return <div>{this.state.resource.length}</div>;
 	}
 }
 
